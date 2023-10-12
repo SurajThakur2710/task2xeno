7985d85f6fe247f21659ddfdafd2515d430f9963
SurajThakur2710 <92199392+SurajThakur2710@users.noreply.github.com>
2023-10-12 21:02:26 +0530

    Add files via upload

---
+++ Hotel/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+from . import templates 
+from django.urls.conf import include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.hotelviews),
+    # path('', views.hotelviews),
+    path('Hotels/', views.Hotels),
+    path('book/',views.book,name="book now "),
+    path('Welcome/', views.welcome, name="Welcome page"),
+    path('Aboutus/', views.aboutus, name="About Us" ),
+    path('Contactus/', views.contactus, name="Contact Us" ),
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
